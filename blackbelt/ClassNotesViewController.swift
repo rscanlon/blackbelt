72186f1756c7095135fb6626c219c528f4789c69
rscanlon <rscanlon@wellesley.edu>
2017-03-01 22:05:58 -0500

    adding class notes view controller

---
+++ blackbelt/ClassNotesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ClassNotesViewController.swift
+//  blackbelt
+//
+//  Created by Rebecca Scanlon on 3/1/17.
+//  Copyright © 2017 Rebecca Scanlon. All rights reserved.
+//
+
+import Foundation
